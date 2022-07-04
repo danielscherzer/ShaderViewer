ebeb2bb85f0af65c526a17828190b9e333c42141
Daniel Scherzer <scherzer@rwu.de>
2022-07-04 13:55:46 +0200

    quick commit

--- src/Systems/PersistenceSystem.cs
+++ src/Systems/PersistenceSystem.cs
@@ -1,5 +1,7 @@
 ﻿using OpenTK.Mathematics;
 using OpenTK.Windowing.Desktop;
+using ShaderViewer.Helper;
+using Zenseless.PersistentSettings;
 
 namespace ShaderViewer.Systems
 {
@@ -13,6 +15,13 @@ namespace ShaderViewer.Systems
 			window.Size = monitorSize / 2;
 			window.Location = (monitorSize - window.Size) / 2;
 			//window.WindowState = OpenTK.Windowing.Common.WindowState.Maximized;
+
+			PersistentSettings settings = new();
+			settings.AddFromGetterSetter("location", () => window.Location.FromOpenTK(), t => window.Location = t.ToOpenTKi());
+			settings.AddFromGetterSetter("size", () => window.Size.FromOpenTK(), t => window.Size = t.ToOpenTKi());
+			settings.Load();
+
+			window.Closing += _ => settings.Store();
 		}
 	}
 }
